c3cd7c5734191ae5851a857fdd4be81d35ec9701
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-04-19 04:55:54 +0330

    define a new query to collect data from two collections

--- zibal/transaction/queries.py
+++ zibal/transaction/queries.py
@@ -1,5 +1,7 @@
 from typing import Optional
 
+from datetime import datetime, timedelta
+
 
 class Mode:
     def get_group_id(self):
@@ -156,3 +158,131 @@ class AggregateData:
             }
         )
         return self.pipeline
+
+
+class AggregateDataV2:
+    def __init__(self):
+        self.merchant_id = None
+        self.mode = None
+        self.today = None
+        self.query_type = None
+        self.summary_query_type = None
+        self.pipeline = []
+        self.summary_pipeline = []
+
+    def set_today_date(self, today):
+        self.today = today
+
+    def set_merchant_id(self, merchant_id: Optional[int]):
+        self.merchant_id = merchant_id
+
+    def set_mode(self, mode: str):
+        self.mode = get_mode(mode_name=mode)
+
+    def set_type(self, query_type: str):
+        if query_type == 'count':
+            self.query_type = 1
+            self.summary_query_type = "$count"
+        elif query_type == 'amount':
+            self.query_type = "$amount"
+            self.summary_query_type = "$total_amount"
+        else:
+            raise ValueError('Invalid type. options are `count` and `amount`.')
+
+    def get_pipeline(self):
+        # Reset pipeline
+        self.pipeline = []
+
+        # Stage 0: merchant_id
+        if self.merchant_id is not None:
+            self.pipeline.append({"$match": {"merchant_id": self.merchant_id}})
+
+        # Stage 1: get today's data
+        today_midnight = datetime.strptime(self.today, '%Y-%m-%d').replace(hour=0, minute=0, second=0, microsecond=0)
+        self.pipeline.append(
+            {
+                "$match": {
+                    "created_at": {
+                        "$gte": today_midnight,
+                        "$lt": today_midnight + timedelta(days=1)
+                    }
+                }
+            }
+        )
+
+        # Stage 2: group
+        self.pipeline.append(
+            {
+                "$group": {
+                    "_id": self.mode.get_group_id(),
+                    "value": {
+                        "$sum": self.query_type
+                    }
+                }
+            }
+        )
+
+        # Stage 3: project
+        self.pipeline.append(
+            {
+                "$project": {
+                    "_id": 0,
+                    "key": self.mode.get_project_key(),
+                    "value": 1
+                }
+            }
+        )
+
+        # Stage 4: sort
+        self.pipeline.append(
+            {
+                "$sort": {"key": 1}
+            }
+        )
+        return self.pipeline
+
+    def get_summary_pipeline(self):
+        # Reset pipeline
+        self.summary_pipeline = []
+
+        # Stage 0: merchant_id
+        if self.merchant_id is not None:
+            self.summary_pipeline.append({"$match": {"merchant_id": self.merchant_id}})
+
+        # Stage 1: group
+        self.summary_pipeline.append(
+            {
+                "$group": {
+                    "_id": self.mode.get_group_id(),
+                    "value": {
+                        "$sum": self.summary_query_type
+                    }
+                }
+            }
+        )
+
+        # Stage 2: project
+        self.summary_pipeline.append(
+            {
+                "$project": {
+                    "_id": 0,
+                    "key": self.mode.get_project_key(),
+                    "value": 1
+                }
+            }
+        )
+
+        # Stage 3: Sort by key
+        self.summary_pipeline.append({
+            "$sort": {"key": 1}
+        })
+        return self.summary_pipeline
+
+    def combine_documents(self, transaction_collection, summary_collection):
+        transaction_results = list(transaction_collection.aggregate(self.get_pipeline()))
+        summary_results = list(summary_collection.aggregate(self.get_summary_pipeline()))
+        if transaction_results[0]['key'] == summary_results[-1]['key']:
+            summary_results[-1]['value'] += transaction_results[0]['value']
+            return summary_results
+        else:
+            return [*summary_results, *transaction_results]
