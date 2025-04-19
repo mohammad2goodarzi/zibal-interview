f048be4979d7e6cdefbdd74e9acb2a5c708814b9
mohammad2goodarzi <mohammad2goodarzi@gmail.com>
2025-04-19 04:40:39 +0330

    implement an API for the transaction aggregation

--- zibal/transaction/views.py
+++ zibal/transaction/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import transaction_collection
+from .queries import AggregateData
+from .serializers import AggregatedTransactionSerializer
+
+
+class TransactionListAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        aggregator = AggregateData()
+        merchant_id = request.GET.get('merchant_id')
+        if merchant_id:
+            aggregator.set_merchant_id(int(merchant_id))
+        aggregator.set_mode(request.GET.get('mode'))
+        aggregator.set_type(request.GET.get('type'))
+
+        # Execute the aggregation
+        results = transaction_collection.aggregate(aggregator.get_pipeline())
+        serializer = AggregatedTransactionSerializer(results, many=True)
+        return Response(serializer.data)
